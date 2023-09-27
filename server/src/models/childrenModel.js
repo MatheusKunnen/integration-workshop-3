5d0d076977b60f52acd3401ca401f05f090eeb9c
gabriellinke <gh.linke@hotmail.com>
2023-09-26 21:56:34 -0300

    Add first version of models and controllers

---
+++ server/src/models/childrenModel.js
@@ -0,0 +1,60 @@
+import { Sequelize } from "sequelize";
+import db from "../database.js";
+import Snacks from './snacksModel.js';
+
+const Children = db.define("Children", {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  parentId: {
+    type: Sequelize.INTEGER,
+    references: { model: 'Parents', key: 'id' },
+    allowNull: false,
+  },
+  passwordImageId: {
+    type: Sequelize.INTEGER,
+    references: { model: 'Images', key: 'id' },
+    allowNull: false,
+  },
+  passwordGroupId: {
+    type: Sequelize.INTEGER,
+    references: { model: 'PasswordGroups', key: 'id' },
+    allowNull: false,
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  tagNumber: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  budget: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    defaultValue: 0,
+  },
+  isBlocked: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false,
+    defaultValue: false,
+  }
+});
+
+export default Children;
+
+Children.belongsToMany(Snacks, { through: 'ChildAllowedSnacks' });
+Snacks.belongsToMany(Children, { through: 'ChildAllowedSnacks' });
+
+const ChildSnackOrders = db.define('ChildSnackOrders', {
+  Price: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  }
+});
+
+Children.belongsToMany(Snacks, { through: ChildSnackOrders });
+Snacks.belongsToMany(Children, { through: ChildSnackOrders });
