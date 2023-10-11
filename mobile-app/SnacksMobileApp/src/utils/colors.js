ae3347c8b5791aa9b3507a96dc15a6a37077c7da
Alessandra F. Lacerda <flacerdaale@gmail.com>
2023-10-10 22:14:46 -0300

    Adds custom colors

---
+++ mobile-app/SnacksMobileApp/src/utils/colors.js
@@ -0,0 +1,8 @@
+// colors.js
+
+export const beige = '#FFECD8';
+export const lightPurple = '#BFC3FF'; 
+export const purple = '#A3A4FF'; 
+export const darkGray = '#1C1A27'; 
+export const lightGray = '#767484';
+export const red = '#FF4970';
