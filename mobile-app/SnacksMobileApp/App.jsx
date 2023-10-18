08405a929e857a5a6edde47e25c6457dd94c2a0a
Alessandra F. Lacerda <flacerdaale@gmail.com>
2023-10-17 23:11:31 -0300

    Creates auth context for login information

--- mobile-app/SnacksMobileApp/App.jsx
+++ mobile-app/SnacksMobileApp/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import { AuthProvider } from './src/AuthContext';
 import Login from './src/screens/Login';
 import Home from './src/screens/Home';
 import ParentRegister from './src/screens/ParentRegister';
@@ -12,19 +13,21 @@ const Stack = createNativeStackNavigator();
 
 function App() {
     return (
-        <NavigationContainer>
-            <Stack.Navigator
-                initialRouteName="Login"
-                screenOptions={{
-                    headerShown: false
-                }}>
-                <Stack.Screen name="Login" component={Login} />
-                <Stack.Screen name="ParentRegister" component={ParentRegister} />
-                <Stack.Screen name="Home" component={Home} />
-                <Stack.Screen name="ManageChildAccount" component={ManageChildAccount} />
-                <Stack.Screen name="AccountDetails" component={AccountDetails} />
-            </Stack.Navigator>
-        </NavigationContainer>
+        <AuthProvider>
+            <NavigationContainer>
+                <Stack.Navigator
+                    initialRouteName="Login"
+                    screenOptions={{
+                        headerShown: false
+                    }}>
+                    <Stack.Screen name="Login" component={Login} />
+                    <Stack.Screen name="ParentRegister" component={ParentRegister} />
+                    <Stack.Screen name="Home" component={Home} />
+                    <Stack.Screen name="ManageChildAccount" component={ManageChildAccount} />
+                    <Stack.Screen name="AccountDetails" component={AccountDetails} />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </AuthProvider>
     );
 }
 
