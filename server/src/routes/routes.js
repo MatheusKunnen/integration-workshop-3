6f8c3a556ece996116e6632554147ae581a7a244
Matheus Kunnen Ledesma <matheuskl@accionproactiva.com>
2023-10-28 12:07:37 -0300

    Change /snacks/purchase/:snack_id http method

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -1,11 +1,11 @@
-import express from "express";
-import snacks from "../app/controllers/snacks.js";
-import passwordGroups from "../app/controllers/passwordGroups.js";
-import verifyToken from '../middlewares/auth.js'
+import express from 'express';
+import snacks from '../app/controllers/snacks.js';
+import passwordGroups from '../app/controllers/passwordGroups.js';
+import verifyToken from '../middlewares/auth.js';
 
 const routes = express.Router();
 
-routes.get("/password-groups/random", passwordGroups.getRandom);
-routes.get("/snacks/purchase/:snack_id", verifyToken, snacks.purchase);
+routes.get('/password-groups/random', passwordGroups.getRandom);
+routes.post('/snacks/purchase/:snack_id', verifyToken, snacks.purchase);
 
 export { routes as default };
