829ca393a8f3b8b2b508751c9ca361f3278db62a
Matheus Kunnen Ledesma <matheuskl@accionproactiva.com>
2023-10-28 18:12:32 -0300

    Add limiter switch implementation

---
+++ vending-machine/motion-system/MotionSystemController/LimiterSwitch.py
@@ -0,0 +1,37 @@
+try:
+    import RPi.GPIO as GPIO
+except:
+    import Mock.GPIO as GPIO
+
+class LimiterSwitch:
+    DEBUG = False
+
+    def __init__(self, pin:int):
+        self.__pin = pin
+        self.__observers = []
+        self.__status: bool = False
+        self.__setup()
+
+    def __setup(self):
+        GPIO.add_event_detect(self.__pin, GPIO.BOTH, 
+            callback=self.__on_change_cb, bouncetime=50)
+
+    def __on_change_cb(self):
+        if self.DEBUG:
+            print(f'Switch changed')
+
+        new_status = True if GPIO.input(self.__pin) else False
+
+        if self.__status != new_status:
+            self.__status = new_status
+            self.__notify_observers
+
+    def __notify_observers(self):
+        for observer in self.__observers:
+            observer(self.__status)
+
+    def is_on(self):
+        return self.__status
+    
+    def add_observer(self, observer):
+        self.__observers.append(observer) 
