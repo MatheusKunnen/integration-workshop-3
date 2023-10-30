a86ad9144cdf3be556b6d14fadb624e9e412782f
Matheus Kunnen Ledesma <matheuskl@accionproactiva.com>
2023-10-30 18:37:54 -0300

    Add compatibility with python < 3.10

--- vending-machine/motion-system/NFCReader/NFCReaderConfiguration.py
+++ vending-machine/motion-system/NFCReader/NFCReaderConfiguration.py
@@ -2,5 +2,5 @@ from dataclasses import dataclass
 
 @dataclass
 class NFCReaderConfiguration:
-    serial_port: str | None = '/dev/ttyACM0'
+    serial_port: str = '/dev/ttyACM0'
     baudrate:int = 115200
