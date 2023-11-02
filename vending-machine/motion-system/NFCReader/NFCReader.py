0abc1befed694c751b6297e52b9c0e578ec2aa87
Matheus Kunnen Ledesma <matheuskl@accionproactiva.com>
2023-11-02 17:31:10 -0300

    Fix bug in NFCReader

--- vending-machine/motion-system/NFCReader/NFCReader.py
+++ vending-machine/motion-system/NFCReader/NFCReader.py
@@ -1,6 +1,6 @@
 from serial import Serial
 from multiprocessing import Process, Queue
-
+from time import sleep
 from .NFCReaderConfiguration import *
 
 class NFCReader:
@@ -13,11 +13,11 @@ class NFCReader:
         self.__thread = None
         self.queue = Queue()
         self.__last_readed = None
-        self.__setup()
+        # self.__setup()
 
-    def __setup(self):
-        if self.__config.serial_port is not None:
-            self.__serial = Serial(self.__config.serial_port, self.__config.baudrate)
+    # def __setup(self):
+        # if self.__config.serial_port is not None:
+            # self.__serial = Serial(self.__config.serial_port, self.__config.baudrate)
 
     def set_callback(self, callback):
         self.__callback = callback
@@ -26,15 +26,15 @@ class NFCReader:
         if self.__thread is not None:
             raise RuntimeError('NFCReader thread already started')
         
-        self.__thread = Process(target=self.__run, name='NFC Reader Thread', daemon=True)
+        self.__thread = Process(target=self.__run, name='NFC Reader Thread')
         
-        if self.__serial is not None:
-            self.__thread.start()
+        self.__thread.start()
 
     def __run(self):
         print('NFC Reader thread started')
-        try:
-            while True:
+        while True:
+            try:
+                self.__serial = Serial(self.__config.serial_port, self.__config.baudrate)
                 raw_data = self.__serial.readline()
                 nfc_code = raw_data.decode().replace('\r','').replace('\n','').lstrip()
 
@@ -46,5 +46,9 @@ class NFCReader:
                     self.queue.put_nowait(nfc_code)
                 
                 # self.__callback(nfc_code)
-        except Exception as e:
-            print(e)
+            except Exception as e:
+                if self.__serial is not None:
+                    self.__serial.close()
+                    self.__serial = None
+                print(e)
+                sleep(3)
